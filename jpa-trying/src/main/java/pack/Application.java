3b0ab5afdce72e2454872ab866ad3aba1d2bf92c
MishLetuchii <Luptax@mail.ru>
2020-07-21 16:41:56 +0300

    Сhanging annotation in app

--- jpa-trying/src/main/java/pack/Application.java
+++ jpa-trying/src/main/java/pack/Application.java
@@ -1,31 +1,16 @@
 package pack;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import pack.domain.User;
-import pack.repositories.UsersRepository;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import java.util.Optional;
 
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
 
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-
-        UsersRepository usersRepository = context.getBean(UsersRepository.class);
-/*
-         Optional<User> us = usersRepository.findById(1092L);
-         User user = us.get();
-         usersRepository.save(user);
-*/
-        System.out.println( usersRepository.findAll());
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class);
 
     }
 
