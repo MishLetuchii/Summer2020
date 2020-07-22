cc79d3b01797d68875ec2b71f50caf2499eafc7d
MishLetuchii <Luptax@mail.ru>
2020-07-22 11:04:09 +0300

    Path Fixes again

--- src/main/java/pack/configuration/WebSecurityConfig.java
+++ src/main/java/pack/configuration/WebSecurityConfig.java
@@ -43,7 +43,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
                 .logout()
+<<<<<<< Updated upstream:src/main/java/pack/configuration/WebSecurityConfig.java
                 .logoutSuccessUrl("/")
+=======
+<<<<<<< HEAD:src/main/java/pack/configuration/WebSecurityConfig.java
+                .logoutSuccessUrl("/main")
+=======
+                .logoutSuccessUrl("/")
+>>>>>>> 2e0c524a2a074a84d1dcc3d0d39df61bc45268c0:jpa-trying/src/main/java/pack/configuration/WebSecurityConfig.java
+>>>>>>> Stashed changes:jpa-trying/src/main/java/pack/configuration/WebSecurityConfig.java
                 .permitAll()
                 .and()
                 .exceptionHandling().accessDeniedPage("/access-error");
